fb0852ac9b4cd69f4145fb61f72dc69accf93e8c
santorfo <39157156+santorfo@users.noreply.github.com>
2019-11-21 18:33:09 +0000

    cp1252 saves the day

--- BitStream.py
+++ BitStream.py
@@ -1,19 +1,29 @@
+import math
+
 class BitStream:
     def __init__(self):
         self.bit_array = None
 
     def read_file(self, filename):
         file = open(filename, 'rb')
-        fcontent = file.read()
+        fcontent = file.read().decode('cp1252')
         self.bit_array = ''.join(format(ord(byte), '08b') for byte in str(fcontent))
-        print(self.bit_array)
 
         #print(len(self.bit_array))
 
         file.close()
 
     def read_n_bits(self, num):
-        pass
+        print(self.bit_array[0:num])
+
+    def write_bits_to_file(self, filename):
+        file = open(filename, 'wb')
+        num_bytes = math.ceil(len(self.bit_array) / 8)
+        bytes = int(self.bit_array, 2).to_bytes(num_bytes, byteorder='big', signed=True)
+        print(bytes)
+
+        file.write(bytes)
+
 
     def write_file(self, filename):
         file = open(filename, 'wb')
@@ -27,5 +37,7 @@ class BitStream:
 if __name__ == '__main__':
 
     bs = BitStream()
-    bs.write_file('test_b')
-    bs.read_file('test_b')
+    #bs.write_file('test_b')
+    bs.read_file('metade.pbm')
+    bs.read_n_bits(30)
+    bs.write_bits_to_file('test_c')
