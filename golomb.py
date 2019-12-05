015b5617c221128c4c28dd877b8eac69b3099524
Daniel Pinto <dmatiaspinto@ua.pt>
2019-12-05 17:56:35 +0000

    golomb

--- golomb.py
+++ golomb.py
@@ -1,32 +1,52 @@
-import sys
-from unary_coding import inverted_unary
-from minimal_binary_coding import minimal_binary_coding
+import os
+import math
 
 class Golomb():
-    def __init__(self, file, b):
-        self.file = file
-        self.b = b
-
-    def read_file(self):
-        f = open(self.file, 'rb')
-        fcontent = f.read().decode('cp1252')
-        bit_array = ''.join(format(ord(byte), '08b') for byte in str(fcontent))
-
-        bi=int(bit_array)
-        #print(bi)
-
-        f.close()
-        self.golomb_encode(12662531652349)
-
-    def golomb_encode(self, bits):
-        print(inverted_unary(bits // self.b)+minimal_binary_coding(bits % self.b, self.b))
+    def __init__(self, m, filename):
+        self.m = m
+        self.filename = filename
+        self.code = []
+        
+                
+    def golomb_encode(self):
+        with open(self.filename, 'rb') as f:
+            for l in range(os.path.getsize(self.filename)):
+                byte = ord(f.read(1))
+                quot = byte // self.m
+                rest = byte % self.m
+                
+                for i in range(int(math.log2(self.m))):
+                    self.code.append((rest >> i) & 1)
+                
+                self.code = self.code + ([1] * quot + [0])
+                
 
         
     def golomb_decode(self):
-        pass
+        size_resto=int(math.log2(self.m))
+        resto_bi=''
+        quociente=0
+
+        for l in self.code:
+            
+            if len(resto_bi) == size_resto:
+                resto=int(resto_bi[::-1], 2)
+
+                if l == 0:
+                    byte = self.m * quociente + resto
+                    
+                    resto_bi=''
+                    quociente=0
+                    
+                else:
+                    quociente += 1
+
+            else:
+                resto_bi=resto_bi+str(l)
+
 
 if __name__ == '__main__':
-    b=120000000
-    gol = Golomb('teste.txt', b)
+    gol = Golomb(16, 'teste.txt')
     
-    gol.read_file()
+    gol.golomb_encode()
+    gol.golomb_decode()
